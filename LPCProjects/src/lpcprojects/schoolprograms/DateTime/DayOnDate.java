e96769c99e2317eb62899f0f5a8e5b4a269de5a6
Abhishek Munie <abhishekmunie@yahoo.co.in>
2010-02-20 12:52:28 +0000

    I have added some programs

--- LPCProjects/src/lpcprojects/schoolprograms/DateTime/DayOnDate.java
+++ LPCProjects/src/lpcprojects/schoolprograms/DateTime/DayOnDate.java
@@ -3,6 +3,7 @@ package lpcprojects.schoolprograms.DateTime;
 import java.io.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -31,94 +32,41 @@ public class DayOnDate {
 	  * @throws IOException
 	  */
 	 static void input() throws IOException {
-			System.out.print("Date : ");
+			System.out.print("Date (dd/mm/yyyy) : ");
 			date = bufferedReader.readLine();
 	 }
 
 	 static void compute() {
-			int idxd = date.indexOf('/');
-			mm = Integer.parseInt(date.substring(0, idxd));
-			if ((mm > 12) && (mm < 1)) {
-				 System.out.println("Invalid Month!");
-				 System.exit(0);
-			}
-			int idxm = date.indexOf('/', (idxd + 1));
-			dd = Integer.parseInt(date.substring((idxd + 1), idxm));
-			yyyy = Integer.parseInt(date.substring(idxm + 1));
-			int maxDay = 0;
-			switch (mm) {
-				 case 1:
-						maxDay = 31;
-						break;
-				 case 2:
-						if (new GregorianCalendar().isLeapYear(yyyy)) {
-							 maxDay = 29;
-						} else {
-							 maxDay = 28;
-						}
-						break;
-				 case 3:
-						maxDay = 31;
-						break;
-				 case 4:
-						maxDay = 30;
-						break;
-				 case 5:
-						maxDay = 31;
-						break;
-				 case 6:
-						maxDay = 30;
-						break;
-				 case 7:
-						maxDay = 31;
-						break;
-				 case 8:
-						maxDay = 31;
-						break;
-				 case 9:
-						maxDay = 30;
-						break;
-				 case 10:
-						maxDay = 31;
-						break;
-				 case 11:
-						maxDay = 30;
-						break;
-				 case 12:
-						maxDay = 31;
-						break;
-				 default:
-						System.out.println("Invalid Month!");
-						System.exit(0);
-			}
-			if (dd > maxDay) {
-				 System.out.println("Invalid Day!");
-				 System.exit(0);
-			}
-			Calendar c = new GregorianCalendar();
-			c.set(yyyy, mm, dd);
+			StringTokenizer stringTokenizer = new StringTokenizer(date, "/");
+			dd = Integer.parseInt(stringTokenizer.nextToken());
+			mm = Integer.parseInt(stringTokenizer.nextToken());
+			yyyy = Integer.parseInt(stringTokenizer.nextToken());
+			Calendar c = Calendar.getInstance();
+			c.set(Calendar.DAY_OF_MONTH, dd);
+			c.set(Calendar.MONTH, (mm - 1));
+			c.set(Calendar.YEAR, yyyy);
 			System.out.print("Day on " + date + " : ");
 			switch (c.get(Calendar.DAY_OF_WEEK)) {
 				 case Calendar.MONDAY:
-						System.out.println("MONDAY");
+						System.out.print("MONDAY");
 						break;
 				 case Calendar.TUESDAY:
-						System.out.println("TUESDAY");
+						System.out.print("TUESDAY");
 						break;
 				 case Calendar.WEDNESDAY:
-						System.out.println("WEDSENDAY");
+						System.out.print("WEDNESDAY");
 						break;
 				 case Calendar.THURSDAY:
-						System.out.println("THURSDAY");
+						System.out.print("THURSDAY");
 						break;
 				 case Calendar.FRIDAY:
-						System.out.println("FRIDAY");
+						System.out.print("FRIDAY");
 						break;
 				 case Calendar.SATURDAY:
-						System.out.println("SATUARDAY");
+						System.out.print("SATURDAY");
 						break;
 				 case Calendar.SUNDAY:
-						System.out.println("SUNDAY");
+						System.out.print("SUNDAY");
 						break;
 			}
 	 }
